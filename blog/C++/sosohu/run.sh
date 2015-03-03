c904c892e5bb8f41861006f46d340abfea98d4de
sosohu <ustc.sosohu@gmail.com>
2015-03-03 21:55:07 +0800

    adjus sosohu's read note

---
+++ blog/C++/sosohu/run.sh
@@ -0,0 +1,7 @@
+echo "#C++ Primer 读书笔记" > README.md
+for (( i=1; i<16; i++)); do
+	head "chap$i.md" -n 1 >> README.md
+done
+
+cat README.md | awk '(NR < 2){print $0} (NR > 1){ print "**Chapter "NR-1".**[*"$1"*](chap"NR-1".md)"}' > tmp
+mv tmp README.md
