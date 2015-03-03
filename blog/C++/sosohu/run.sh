a582514694fd208ac15cb17476eb48c4f4a199f9
sosohu <ustc.sosohu@gmail.com>
2015-03-03 21:59:08 +0800

    mini adjust

--- blog/C++/sosohu/run.sh
+++ blog/C++/sosohu/run.sh
@@ -3,5 +3,5 @@ for (( i=1; i<16; i++)); do
 	head "chap$i.md" -n 1 >> README.md
 done
 
-cat README.md | awk '(NR < 2){print $0} (NR > 1){ print "**Chapter "NR-1".**[*"$1"*](chap"NR-1".md)"}' > tmp
+cat README.md | awk '(NR < 2){print $0} (NR > 1){ print "**Chapter "NR-1".**[*"$1"*](chap"NR-1".md)\n"}' > tmp
 mv tmp README.md
